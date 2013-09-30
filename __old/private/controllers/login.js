f2c1806c3165182a0ea886c7c8f0dcbfdd9f61c2
martin <martin@flip.(none)>
2013-09-30 03:51:04 +0200

    Deleted __old

--- __old/private/controllers/login.js
+++
@@ -1,9 +0,0 @@
-exports.render = function (req, res) {
-    var strings = {
-        "Provider login failed": res.__("Provider login failed")
-    };
-
-    res.render("login", {
-        strings: JSON.stringify(strings)
-    });
-};
