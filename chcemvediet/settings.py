baf23165fc5732132219a00f091a6639ab0a32e4
martin <martin@flip.(none)>
2013-10-01 05:29:18 +0200

    bootstrap

--- chcemvediet/settings.py
+++ chcemvediet/settings.py
@@ -141,6 +141,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.webdesign',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
