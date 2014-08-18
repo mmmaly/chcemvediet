8dab583732a4cb80c7f5deb30bc219591868c343
Martin Macko <martin@bitbucket.ksp.sk>
2014-08-18 05:43:43 +0200

    translared url

--- chcemvediet/apps/applications/urls.py
+++ chcemvediet/apps/applications/urls.py
@@ -1,11 +1,12 @@
 # vim: expandtab
 from django.conf.urls import patterns, url
+from django.utils.translation import ugettext_lazy as _
 
 import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^create/$', views.create, name='create'),
-    url(r'^detail/(?P<application_id>\d+)/$', views.detail, name='detail'),
+    url(_(r'^create/$'), views.create, name='create'),
+    url(_(r'^detail/(?P<application_id>\d+)/$'), views.detail, name='detail'),
 )
 
--- chcemvediet/urls.py
+++ chcemvediet/urls.py
@@ -1,8 +1,9 @@
 # vim: expandtab
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 import views
 
@@ -14,11 +15,11 @@ urlpatterns = patterns('',
 
 urlpatterns += i18n_patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
+    url(_(r'^about/'), views.about, name='about'),
+    url(_(r'^obligees/'), include('chcemvediet.apps.obligees.urls', namespace='obligees')),
+    url(_(r'^applications/'), include('chcemvediet.apps.applications.urls', namespace='applications')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^accounts/', include('chcemvediet.apps.accounts.urls', namespace='accounts')),
-    url(r'^obligees/', include('chcemvediet.apps.obligees.urls', namespace='obligees')),
-    url(r'^applications/', include('chcemvediet.apps.applications.urls', namespace='applications')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 )
