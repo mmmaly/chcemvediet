a4e7636a5a739809207c15b76806481f7f866238
Martin Macko <martin@bitbucket.ksp.sk>
2014-10-19 15:19:04 +0200

    poleno.utils.date unittests

--- poleno/utils/date.py
+++ poleno/utils/date.py
@@ -1,6 +1,7 @@
 # vim: expandtab
 # -*- coding: utf-8 -*-
 import datetime
+import pytz
 
 from django.utils import timezone
 
@@ -9,28 +10,26 @@ from django.utils import timezone
 def local_now(tz=None):
     u"""
     Returns aware ``datetime`` in timezone ``tz``, by default the current timezone, representing
-    current time.
+    current time. The timezone may be given as a ``tzinfo`` instance or a string understood by
+    ``pytz``.
 
     Usage:
         local_now([tz])
     """
+    tz = pytz.timezone(tz) if isinstance(tz, basestring) else tz
     dt = timezone.localtime(timezone.now(), tz)
     return dt
 
-def local_datetime(*args, **kwargs):
+def local_datetime(dt, tz=None):
     u"""
     Returns aware ``datetime`` in timezone ``tz``, by default the current timezone, representing
-    the same point in time as the given day and time. The day and time may be given as an aware
-    ``datetime`` or a combination of naive ``date`` and aware ``time``. The naive ``date`` may be
-    given in its expanded form.
+    the same point in time as the given aware datetime ``dt``. The timezone may be given as
+    a ``tzinfo`` instance or a string understood by ``pytz``.
 
     Usage:
         local_datetime(aware_datetime, [tz])
-        local_datetime(date, aware_time, [tz])
-        local_datetime(year, month, day, aware_time, [tz])
     """
-    tz = kwargs.pop(u'tz', None)
-    dt = _datetime_factory(*args, **kwargs)
+    tz = pytz.timezone(tz) if isinstance(tz, basestring) else tz
     assert timezone.is_aware(dt)
     dt = timezone.localtime(dt, tz)
     return dt
@@ -39,22 +38,25 @@ def local_datetime_from_local(*args, **kwargs):
     u"""
     Returns aware ``datetime`` in timezone ``tz``, by default the current timezone, representing
     the same point in time as the given day and time. The day and time may be given as a naive
-    ``datetime`` or a combination of naive ``date`` and naive ``time``. Both naive ``date`` and
-    naive ``time`` may be given in their expanded forms. The given point in time is assumed to be
-    in timezone ``from_tz``, by default the current timezone.
+    ``datetime`` or a combination of ``date`` and ``time``. Both ``date`` and ``time`` may be given
+    in their expanded forms. The given point in time is assumed to be in timezone ``from_tz``, by
+    default the current timezone. Both timezones may be given as ``tzinfo`` instances or strings
+    understood by ``pytz``.
 
     Usage:
         local_datetime_from_local(naive_datetime, [from_tz], [tz])
-        local_datetime_from_local(date, naive_time, [from_tz], [tz])
+        local_datetime_from_local(date, time, [from_tz], [tz])
         local_datetime_from_local(date, [hour], [minute], [second], [microsecond], [from_tz], [tz])
-        local_datetime_from_local(year, month, day, naive_time, [from_tz], [tz])
+        local_datetime_from_local(year, month, day, time, [from_tz], [tz])
         local_datetime_from_local(year, month, day, [hour], [minute], [second], [microsecond], [from_tz], [tz])
     """
     from_tz = kwargs.pop(u'from_tz', timezone.get_current_timezone())
+    from_tz = pytz.timezone(from_tz) if isinstance(from_tz, basestring) else from_tz
     tz = kwargs.pop(u'tz', None)
+    tz = pytz.timezone(tz) if isinstance(tz, basestring) else tz
     dt = _datetime_factory(*args, **kwargs)
     assert timezone.is_naive(dt)
-    dt = dt.replace(tzinfo=from_tz)
+    dt = timezone.make_aware(dt, from_tz)
     dt = timezone.localtime(dt, tz)
     return dt
 
@@ -62,15 +64,15 @@ def local_datetime_from_utc(*args, **kwargs):
     u"""
     Returns aware ``datetime`` in timezone ``tz``, by default the current timezone, representing
     the same point in time as the given day and time. The day and time may be given as a naive
-    ``datetime`` or a combination of naive ``date`` and naive ``time``. Both naive ``date`` and
-    naive ``time`` may be given in their expanded forms. The given point in time is assumed to be
-    in UTC.
+    ``datetime`` or a combination of ``date`` and ``time``. Both ``date`` and ``time`` may be given
+    in their expanded forms. The given point in time is assumed to be in UTC. The timezone may be
+    given as a ``tzinfo`` instance or a string understood by ``pytz``.
 
     Usage:
         local_datetime_from_utc(naive_datetime, [tz])
-        local_datetime_from_utc(date, naive_time, [tz])
+        local_datetime_from_utc(date, time, [tz])
         local_datetime_from_utc(date, [hour], [minute], [second], [microsecond], [tz])
-        local_datetime_from_utc(year, month, day, naive_time, [tz])
+        local_datetime_from_utc(year, month, day, time, [tz])
         local_datetime_from_utc(year, month, day, [hour], [minute], [second], [microsecond], [tz])
     """
     return local_datetime_from_local(from_tz=timezone.utc, *args, **kwargs)
@@ -84,32 +86,29 @@ def utc_now():
     """
     return timezone.now()
 
-def utc_datetime(*args, **kwargs):
+def utc_datetime(dt):
     u"""
-    Returns aware ``datetime`` in UTC representing the same point in time as the given day and
-    time. The day and time may be given as an aware ``datetime`` or a combination of naive ``date``
-    and aware ``time``. The naive ``date`` may be given in its expanded form.
+    Returns aware ``datetime`` in UTC representing the same point in time as the given aware
+    datetime ``dt``.
 
     Usage:
         utc_datetime(aware_datetime)
-        utc_datetime(date, [aware_time)
-        utc_datetime(year, month, day, [aware_time])
     """
-    return local_datetime(tz=timezone.utc, *args, **kwargs)
+    return local_datetime(dt, timezone.utc)
 
 def utc_datetime_from_local(*args, **kwargs):
     u"""
     Returns aware ``datetime`` in UTC representing the same point in time as the given day and
-    time. The day and time may be given as a naive ``datetime`` or a combination of naive ``date``
-    and naive ``time``. Both naive ``date`` and naive ``time`` may be given in their expanded
-    forms. The given point in time is assumed to be in timezone ``from_tz``, by default the current
-    timezone.
+    time. The day and time may be given as a naive ``datetime`` or a combination of ``date`` and
+    ``time``. Both ``date`` and ``time`` may be given in their expanded forms. The given point in
+    time is assumed to be in timezone ``from_tz``, by default the current timezone. The timezone
+    may be given as a ``tzinfo`` instance or a string understood by ``pytz``.
 
     Usage:
         utc_datetime_from_local(naive_datetime, [from_tz])
-        utc_datetime_from_local(date, naive_time, [from_tz])
+        utc_datetime_from_local(date, time, [from_tz])
         utc_datetime_from_local(date, [hour], [minute], [second], [microsecond], [from_tz])
-        utc_datetime_from_local(year, month, day, naive_time, [from_tz])
+        utc_datetime_from_local(year, month, day, time, [from_tz])
         utc_datetime_from_local(year, month, day, [hour], [minute], [second], [microsecond], [from_tz])
     """
     return local_datetime_from_local(tz=timezone.utc, *args, **kwargs)
@@ -117,15 +116,15 @@ def utc_datetime_from_local(*args, **kwargs):
 def utc_datetime_from_utc(*args, **kwargs):
     u"""
     Returns aware ``datetime`` in UTC representing the same point in time as the given day and
-    time. The day and time may be given as a naive ``datetime`` or a combination of naive ``date``
-    and naive ``time``. Both naive ``date`` and naive ``time`` may be given in their expanded
-    forms. The given point in time is assumed to be in UTC.
+    time. The day and time may be given as a naive ``datetime`` or a combination of ``date`` and
+    ``time``. Both ``date`` and ``time`` may be given in their expanded forms. The given point in
+    time is assumed to be in UTC.
 
     Usage:
         utc_datetime_from_utc(naive_datetime)
-        utc_datetime_from_utc(date, naive_time)
+        utc_datetime_from_utc(date, time)
         utc_datetime_from_utc(date, [hour], [minute], [second], [microsecond])
-        utc_datetime_from_utc(year, month, day, naive_time)
+        utc_datetime_from_utc(year, month, day, time)
         utc_datetime_from_utc(year, month, day, [hour], [minute], [second], [microsecond])
     """
     return local_datetime_from_local(from_tz=timezone.utc, tz=timezone.utc, *args, **kwargs)
@@ -134,24 +133,23 @@ def utc_datetime_from_utc(*args, **kwargs):
 def local_today(tz=None):
     u"""
     Returns ``date`` representing current day in timezone ``tz``, by default the current timezone.
+    The timezone may be given as a ``tzinfo`` instance or a string understood by ``pytz``.
 
     Usage:
         local_today([tz])
     """
-    return timezone.localtime(timezone.now(), tz).date()
+    return local_now(tz).date()
 
-def local_date(*args, **kwargs):
+def local_date(dt, tz=None):
     u"""
     Returns ``date`` part of the given point in time converted to timezone ``tz``, by default the
-    current timezone. The point in time may be given as an aware ``datetime`` or a combination of
-    naive ``date`` and aware ``time``. The naive ``date`` may be given in its expanded form.
+    current timezone. The point in time is given as an aware ``datetime``. The timezone may be
+    given as a ``tzinfo`` instance or a string understood by ``pytz``.
 
     Usage:
         local_date(aware_datetime, [tz])
-        local_date(date, aware_time, [tz])
-        local_date(year, month, day, aware_time, [tz])
     """
-    return local_datetime(*args, **kwargs).date()
+    return local_datetime(dt, tz).date()
 
 def utc_today():
     u"""
@@ -160,33 +158,30 @@ def utc_today():
     Usage:
         utc_today()
     """
-    return timezone.now().date()
+    return utc_now().date()
 
-def utc_date(*args, **kwargs):
+def utc_date(dt):
     u"""
-    Returns ``date`` part of the given point in time converted to UTC. The point in time may be
-    given as an aware ``datetime`` or a combination of naive ``date`` and aware ``time``. The naive
-    ``date`` may be given in its expanded form.
+    Returns ``date`` part of the given point in time converted to UTC. The point in time is given
+    as an aware ``datetime``.
 
     Usage:
-        utc_date(naive_datetime)
-        utc_date(date, naive_time)
-        utc_date(month, day, aware_time, naive_time)
+        utc_date(aware_datetime)
     """
-    return utc_datetime(*args, **kwargs).date()
+    return utc_datetime(dt).date()
 
 def naive_date(*args, **kwargs):
     u"""
-    Returns naive ``date`` representing the same day as the date part of the given day and time.
-    The day and time may be given as a naive ``datetime`` or a combination of naive ``date`` and
-    naive ``time``. Both naive ``date`` and naive ``time`` may be given in their expanded forms.
+    Returns ``date`` representing the same day as the date part of the given day and time.
+    The day and time may be given as a naive ``datetime`` or a combination of ``date`` and
+    ``time``. Both ``date`` and ``time`` may be given in their expanded forms.
     The time part of the given value is ignored.
 
     Usage:
         naive_date(naive_datetime)
-        naive_date(date, naive_time)
+        naive_date(date, time)
         naive_date(date, [hour], [minute], [second], [microsecond])
-        naive_date(year, month, day, naive_time)
+        naive_date(year, month, day, time)
         naive_date(year, month, day, [hour], [minute], [second], [microsecond])
     """
     dt = _datetime_factory(*args, **kwargs)
@@ -215,7 +210,9 @@ def _datetime_factory(*args, **kwargs):
             return args.pop(0)
         if default is not nop:
             return default
-        raise TypeError(u'Expecting argument: %s' % (error or name))
+        if not error:
+            error = name
+        raise TypeError(u'Expecting argument: %s' % error)
 
     dt = pop_arg(u'datetime', datetime.datetime, default=None)
     if dt is None:
@@ -227,7 +224,7 @@ def _datetime_factory(*args, **kwargs):
             date = datetime.date(year, month, day)
         time = pop_arg(u'time', datetime.time, default=None)
         if time is None:
-            hour = pop_arg(u'hour', int, default=0, error=u'time or hour')
+            hour = pop_arg(u'hour', int, default=0)
             minute = pop_arg(u'minute', int, default=0)
             second = pop_arg(u'second', int, default=0)
             microsecond = pop_arg(u'microsecond', int, default=0)
---
+++ poleno/utils/tests/test_date.py
@@ -0,0 +1,282 @@
+# vim: expandtab
+# -*- coding: utf-8 -*-
+import datetime
+import mock
+import pytz
+from testfixtures import test_datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from poleno.utils.date import local_now, local_datetime, local_datetime_from_local, local_datetime_from_utc
+from poleno.utils.date import utc_now, utc_datetime, utc_datetime_from_local, utc_datetime_from_utc
+from poleno.utils.date import local_today, local_date, utc_today, utc_date, naive_date
+
+class DateTest(TestCase):
+    u"""
+    Tests ``local_*()``, ``utc_*()`` and ``naive_date()`` functions. Checks that computed dates and
+    datetimes are corrent, are of correct types and have correct timezones set.
+    """
+
+    def setUp(self):
+        self.timezone_override = timezone.override(u'Europe/Bratislava') # Fix local timezone
+        self.timezone_override.__enter__()
+
+    def tearDown(self):
+        self.timezone_override.__exit__(None, None, None)
+
+
+    def _parse_dt(self, value, tzname=None):
+        dt = datetime.datetime.strptime(value, u'%Y-%m-%d %H:%M:%S')
+        if tzname is not None:
+            dt = pytz.timezone(tzname).localize(dt)
+        return dt
+
+    def _parse_date(self, value):
+        dt = datetime.datetime.strptime(value, u'%Y-%m-%d')
+        return dt.date()
+
+    def _parse_time(self, value):
+        dt = datetime.datetime.strptime(value, u'%H:%M:%S')
+        return dt.time()
+
+    def _check_dt(self, value, expected):
+        # FIXME: We should check against the class, not its name. After fixing timewarp.
+        self.assertEqual(repr(type(value)), u"<type 'datetime.datetime'>")
+        self.assertEqual(value.strftime(u'%Y-%m-%d %H:%M:%S.%f %Z %z'), expected)
+
+    def _check_date(self, value, expected):
+        # FIXME: We should check against the class, not its name. After fixing timewarp.
+        self.assertEqual(repr(type(value)), u"<type 'datetime.date'>")
+        self.assertEqual(value.strftime(u'%Y-%m-%d'), expected)
+
+
+    # local_now(), local_datetime(), local_datetime_from_local(), local_datetime_from_utc()
+    def test_local_now(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_now()
+            self._check_dt(value, u'2014-09-10 04:20:00.000000 CEST +0200')
+
+    def test_local_now_with_tz_az_tzinfo(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_now(pytz.timezone(u'America/Montreal'))
+            self._check_dt(value, u'2014-09-09 22:20:00.000000 EDT -0400')
+
+    def test_local_now_with_tz_as_string(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_now(u'America/Montreal')
+            self._check_dt(value, u'2014-09-09 22:20:00.000000 EDT -0400')
+
+    def test_local_datetime(self):
+        value = local_datetime(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'))
+        self._check_dt(value, u'2014-12-10 16:45:00.000000 CET +0100')
+
+    def test_local_datetime_with_datetime_in_another_tz(self):
+        value = local_datetime(self._parse_dt(u'2014-12-20 15:45:00', u'America/Montreal'))
+        self._check_dt(value, u'2014-12-20 21:45:00.000000 CET +0100')
+
+    def test_local_datetime_with_tz_as_tzinfo(self):
+        value = local_datetime(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'), pytz.timezone(u'America/Montreal'))
+        self._check_dt(value, u'2014-12-10 10:45:00.000000 EST -0500')
+
+    def test_local_datetime_with_tz_as_string(self):
+        value = local_datetime(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'), u'America/Montreal')
+        self._check_dt(value, u'2014-12-10 10:45:00.000000 EST -0500')
+
+    def test_local_datetime_from_local(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_local_datetime_from_local_with_from_tz_as_tzinfo(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), from_tz=pytz.timezone(u'America/Montreal'))
+        self._check_dt(value, u'2014-08-15 09:45:00.000000 CEST +0200')
+
+    def test_local_datetime_from_local_with_from_tz_as_string(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), from_tz=u'America/Montreal')
+        self._check_dt(value, u'2014-08-15 09:45:00.000000 CEST +0200')
+
+    def test_local_datetime_from_local_with_tz_as_tzinfo(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), tz=pytz.timezone(u'America/Montreal'))
+        self._check_dt(value, u'2014-08-14 21:45:00.000000 EDT -0400')
+
+    def test_local_datetime_from_local_with_tz_as_string(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), tz=u'America/Montreal')
+        self._check_dt(value, u'2014-08-14 21:45:00.000000 EDT -0400')
+
+    def test_local_datetime_from_local_with_both_from_tz_and_tz(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), from_tz=u'America/Montreal', tz=u'Europe/London')
+        self._check_dt(value, u'2014-08-15 08:45:00.000000 BST +0100')
+
+    def test_local_datetime_from_utc(self):
+        value = local_datetime_from_utc(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 05:45:00.000000 CEST +0200')
+
+    def test_local_datetime_from_utc_with_tz_as_tzinfo(self):
+        value = local_datetime_from_utc(self._parse_dt(u'2014-08-15 03:45:00'), tz=pytz.timezone(u'America/Montreal'))
+        self._check_dt(value, u'2014-08-14 23:45:00.000000 EDT -0400')
+
+    def test_local_datetime_from_utc_with_tz_as_string(self):
+        value = local_datetime_from_utc(self._parse_dt(u'2014-08-15 03:45:00'), tz=u'America/Montreal')
+        self._check_dt(value, u'2014-08-14 23:45:00.000000 EDT -0400')
+
+
+    # utc_now(), utc_datetime(), utc_datetime_from_local() and utc_datetime_from_utc()
+    def test_utc_now(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = utc_now()
+            self._check_dt(value, u'2014-09-10 02:20:00.000000 UTC +0000')
+
+    def test_utc_datetime(self):
+        value = utc_datetime(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'))
+        self._check_dt(value, u'2014-12-10 15:45:00.000000 UTC +0000')
+
+    def test_utc_datetime_with_datetime_in_another_tz(self):
+        value = utc_datetime(self._parse_dt(u'2014-12-20 15:45:00', u'America/Montreal'))
+        self._check_dt(value, u'2014-12-20 20:45:00.000000 UTC +0000')
+
+    def test_utc_datetime_from_local(self):
+        value = utc_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 01:45:00.000000 UTC +0000')
+
+    def test_utc_datetime_from_local_with_from_tz_as_tzinfo(self):
+        value = utc_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), from_tz=pytz.timezone(u'America/Montreal'))
+        self._check_dt(value, u'2014-08-15 07:45:00.000000 UTC +0000')
+
+    def test_utc_datetime_from_local_with_from_tz_as_string(self):
+        value = utc_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), from_tz=u'America/Montreal')
+        self._check_dt(value, u'2014-08-15 07:45:00.000000 UTC +0000')
+
+    def test_utc_datetime_from_utc(self):
+        value = utc_datetime_from_utc(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 UTC +0000')
+
+
+    # local_today(), local_date()
+    def test_local_today(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_today()
+            self._check_date(value, u'2014-09-10')
+
+    def test_local_today_with_day_change(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 23, 20)): # in UTC
+            value = local_today()
+            self._check_date(value, u'2014-09-11')
+
+    def test_local_today_with_tz_as_tzinfo(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_today(pytz.timezone(u'America/Montreal'))
+            self._check_date(value, u'2014-09-09')
+
+    def test_local_today_with_tz_as_string(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = local_today(u'America/Montreal')
+            self._check_date(value, u'2014-09-09')
+
+    def test_local_date(self):
+        value = local_date(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'))
+        self._check_date(value, u'2014-12-10')
+
+    def test_local_date_with_day_change(self):
+        value = local_date(self._parse_dt(u'2014-12-10 23:45:00', u'UTC'))
+        self._check_date(value, u'2014-12-11')
+
+    def test_local_date_with_tz_as_tzinfo(self):
+        value = local_date(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'), pytz.timezone(u'America/Montreal'))
+        self._check_date(value, u'2014-12-10')
+
+    def test_local_date_with_tz_as_string(self):
+        value = local_date(self._parse_dt(u'2014-12-10 15:45:00', u'UTC'), u'America/Montreal')
+        self._check_date(value, u'2014-12-10')
+
+
+    # utc_today(), utc_date, naive_date()
+    def test_utc_today(self):
+        with mock.patch(u'django.utils.timezone.datetime', test_datetime(2014, 9, 10, 2, 20)): # in UTC
+            value = utc_today()
+            self._check_date(value, u'2014-09-10')
+
+    def test_utc_date(self):
+        value = utc_date(self._parse_dt(u'2014-12-10 15:45:00', u'Europe/Bratislava'))
+        self._check_date(value, u'2014-12-10')
+
+    def test_utc_date_with_day_change(self):
+        value = utc_date(self._parse_dt(u'2014-12-10 22:45:00', u'America/Montreal'))
+        self._check_date(value, u'2014-12-11')
+
+    def test_naive_date(self):
+        value = naive_date(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_date(value, u'2014-08-15')
+
+
+    # All functions that use _datetime_factory() for passing datetime values.
+    def test_datetime_argument_as_datetime(self):
+        value = local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_datetime_as_kwarg(self):
+        value = local_datetime_from_local(datetime=self._parse_dt(u'2014-08-15 03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_date_and_time(self):
+        value = local_datetime_from_local(self._parse_date(u'2014-08-15'), self._parse_time('03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_date_and_time_as_kwargs(self):
+        value = local_datetime_from_local(date=self._parse_date(u'2014-08-15'), time=self._parse_time('03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_date_and_expanded_time(self):
+        value = local_datetime_from_local(self._parse_date(u'2014-08-15'), 3, 45, 0, 123)
+        self._check_dt(value, u'2014-08-15 03:45:00.000123 CEST +0200')
+
+    def test_datetime_argument_as_date_and_expanded_time_as_kwargs(self):
+        value = local_datetime_from_local(date=self._parse_date(u'2014-08-15'), hour=3, minute=45, second=0, microsecond=123)
+        self._check_dt(value, u'2014-08-15 03:45:00.000123 CEST +0200')
+
+    def test_datetime_argument_as__date_and_default_time(self):
+        value = local_datetime_from_local(self._parse_date(u'2014-08-15'))
+        self._check_dt(value, u'2014-08-15 00:00:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_date_and_default_time_as_kwarg(self):
+        value = local_datetime_from_local(date=self._parse_date(u'2014-08-15'))
+        self._check_dt(value, u'2014-08-15 00:00:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_expanded_date_and_time(self):
+        value = local_datetime_from_local(2014, 8, 15, self._parse_time('03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_expanded_date_and_time_as_kwargs(self):
+        value = local_datetime_from_local(year=2014, month=8, day=15, time=self._parse_time('03:45:00'))
+        self._check_dt(value, u'2014-08-15 03:45:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_expanded_date_and_expaded_time(self):
+        value = local_datetime_from_local(2014, 8, 15, 3, 45, 0, 123)
+        self._check_dt(value, u'2014-08-15 03:45:00.000123 CEST +0200')
+
+    def test_datetime_argument_as__expanded_date_and_expaded_time_as_kwarg(self):
+        value = local_datetime_from_local(year=2014, month=8, day=15, hour=3, minute=45, second=0, microsecond=123)
+        self._check_dt(value, u'2014-08-15 03:45:00.000123 CEST +0200')
+
+    def test_datetime_argument_as_expanded_date_and_default_time(self):
+        value = local_datetime_from_local(2014, 8, 15)
+        self._check_dt(value, u'2014-08-15 00:00:00.000000 CEST +0200')
+
+    def test_datetime_argument_as_expanded_date_and_default_time_as_kwarg(self):
+        value = local_datetime_from_local(year=2014, month=8, day=15)
+        self._check_dt(value, u'2014-08-15 00:00:00.000000 CEST +0200')
+
+    def test_datetime_argument_with_missing_all_arguments(self):
+        with self.assertRaisesMessage(TypeError, u'Expecting argument: datetime, date or year'):
+            local_datetime_from_local()
+
+    def test_datetime_argument_with_missing_argument(self):
+        with self.assertRaisesMessage(TypeError, u'Expecting argument: day'):
+            local_datetime_from_local(2014, 8)
+
+    def test_datetime_argument_with_invalid_argument(self):
+        with self.assertRaisesMessage(TypeError, u'Unexpected arguments: 2014'):
+            local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), 2014)
+
+    def test_datetime_argument_with_invalid_kw_argument(self):
+        with self.assertRaisesMessage(TypeError, u'Unexpected arguments: year=2014'):
+            local_datetime_from_local(self._parse_dt(u'2014-08-15 03:45:00'), year=2014)
