f82beb4fab3bb558151c52fd6863ad4fa1441ca2
Martin Macko <martin@github.ksp.sk>
2015-07-09 02:31:16 +0200

    sitemaps not working, droping

--- chcemvediet/urls.py
+++ chcemvediet/urls.py
@@ -11,7 +11,6 @@ from . import ready
 
 
 urlpatterns = patterns(u'',
-    url(r'^sitemap[.]xml$', u'django.contrib.sitemaps.views.sitemap'),
     url(r'^mandrill/', include(u'poleno.mail.transports.mandrill.urls', namespace=u'mandrill')),
     url(r'^i18n/', include(u'django.conf.urls.i18n')),
 )
